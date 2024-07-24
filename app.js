0f7a468303c6c9fe50be8d93ed9cfe1bc7096e11
qkenn <github.com.sampling990@passinbox.com>
2024-07-24 12:07:41 +0530

    feat: initiate game board & make a move

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log('hooked');
+
+const gameBoard = (function gameBoard() {
+  const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+
+  function playMove(row, col) {
+    for (let i = 1; i <= 3; i++) {
+      for (let j = 1; j <= 3; j++) {
+        if (i == row && j == col) {
+          this.board[row - 1][col - 1] = 'x';
+        }
+      }
+    }
+  }
+
+  return { board, playMove };
+})();
+
+gameBoard.playMove(1, 2);
+gameBoard.playMove(3, 3);
+
+console.log(gameBoard.board);
