818fe2822091b1fea0c2744d1359fb473f88a0a3
qkenn <github.com.sampling990@passinbox.com>
2024-07-24 21:55:22 +0530

    feat: playgame with stats & default players

--- app.js
+++ app.js
@@ -6,73 +6,72 @@ const game = (function game() {
   ];
 
   const currentPlayer = { id: 1, symbol: 'x' };
-
   let gameOver = false;
+  let winner = '';
+  let tie = false;
 
-  return { board, currentPlayer };
+  return { board, currentPlayer, tie, gameOver, winner };
 })();
 
-function player(id, name, symbol) {
-  const changeName = function (newName) {
-    this.name = newName;
-  };
+// function player(id, name, symbol) {
+//   const changeName = function (newName) {
+//     this.name = newName;
+//   };
 
-  return { id, name, symbol, changeName };
-}
+//   return { id, name, symbol, changeName };
+// }
 
 const play = (function () {
-  function checkGameOver() {
-    // checkColumns
-    for (let i = 0; i < 3; i++) {
+  function checkMatch() {
+    const checkCol = function () {
+      for (let i = 0; i < 3; i++) {
+        if (
+          game.board[i][0] == game.board[i][1] &&
+          game.board[i][1] == game.board[i][2] &&
+          game.board[i][0] != ''
+        ) {
+          game.gameOver = true;
+          game.winner = game.currentPlayer.symbol;
+          return true;
+        }
+      }
+    };
+
+    const checkRow = function () {
+      for (let i = 0; i < 3; i++) {
+        if (
+          game.board[0][i] == game.board[1][i] &&
+          game.board[1][i] == game.board[2][i] &&
+          game.board[0][i] != ''
+        ) {
+          game.gameOver = true;
+          game.winner = game.currentPlayer.symbol;
+          return true;
+        }
+      }
+    };
+
+    const checkDiagnol = function () {
       if (
-        game.board[i][0] == game.board[i][1] &&
-        game.board[i][1] == game.board[i][2] &&
-        game.board[i][0] != ''
+        (game.board[0][0] == game.board[1][1] &&
+          game.board[1][1] == game.board[2][2] &&
+          game.board[0][0] != '') ||
+        (game.board[0][2] == game.board[1][1] &&
+          game.board[1][1] == game.board[2][0] &&
+          game.board[0][2] != '')
       ) {
+        game.gameOver = true;
+        game.winner = game.currentPlayer.symbol;
         return true;
       }
-    }
+    };
 
-    // checkRows
-    for (let i = 0; i < 3; i++) {
-      if (
-        game.board[0][i] == game.board[1][i] &&
-        game.board[1][i] == game.board[2][i] &&
-        game.board[0][i] != ''
-      ) {
-        return true;
-      }
-    }
-
-    // checkDiagnols
-    if (
-      (game.board[0][0] == game.board[1][1] &&
-        game.board[1][1] == game.board[2][2] &&
-        game.board[0][0] != '') ||
-      (game.board[0][2] == game.board[1][1] &&
-        game.board[1][1] == game.board[2][0] &&
-        game.board[0][2] != '')
-    ) {
-      return true;
-    }
-
-    return false;
+    if (checkCol()) return true;
+    if (checkRow()) return true;
+    if (checkDiagnol()) return true;
   }
 
-  function changePlayers() {
-    game.currentPlayer.id == 1
-      ? (game.currentPlayer.id = 2)
-      : (game.currentPlayer.id = 1);
-
-    game.currentPlayer.symbol == 'x'
-      ? (game.currentPlayer.symbol = 'o')
-      : (game.currentPlayer.symbol = 'x');
-  }
-
-  function playOneRound(row, col, cell) {
-    if (checkGameOver()) return;
-
-    // mark symbol on the cell
+  const changeGameBoard = function (row, col) {
     for (let i = 1; i <= 3; i++) {
       for (let j = 1; j <= 3; j++) {
         if (i == row && j == col) {
@@ -80,32 +79,63 @@ const play = (function () {
         }
       }
     }
+  };
 
-    displayController.displayMove(cell);
+  const changePlayers = function () {
+    game.currentPlayer.symbol == 'x'
+      ? (game.currentPlayer.symbol = 'o')
+      : (game.currentPlayer.symbol = 'x');
+  };
 
-    if (play.checkGameOver()) {
-      play.getGameStats();
-
+  const makeMove = function (row, col, cell) {
+    if (game.gameOver || game.tie) {
       return;
     }
 
+    play.changeGameBoard(row, col);
+
+    play.checkMatch();
+    play.checkTie();
+
+    if (game.gameOver || game.tie) {
+      displayController.displayStats(
+        `GameOver: ${game.gameOver}, Tie: ${game.tie}, Winner: ${game.winner}`
+      );
+    }
+
+    displayController.displayMove(cell);
     play.changePlayers();
-  }
+  };
 
-  function getGameStats() {
-    console.log(`You won ${game.currentPlayer.symbol}`);
-  }
+  const checkTie = function () {
+    let checkedElements = 0;
 
-  return { checkGameOver, changePlayers, playOneRound, getGameStats };
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (game.board[i][j] != '') checkedElements++;
+      }
+    }
+
+    // tie when all cells are checked but no match
+    if (checkedElements == 9) {
+      game.tie = true;
+      game.gameOver = true;
+      return true;
+    }
+  };
+
+  return {
+    checkMatch,
+    changePlayers,
+    makeMove,
+    checkTie,
+    changeGameBoard,
+  };
 })();
-
-const player1 = player(1, 'erik', 'o');
-const player2 = player(2, 'stan', 'x');
-
-console.log(game.board);
 
 const displayController = (function () {
   const gameBoardEl = document.getElementById('gameboard');
+  const statsEl = document.getElementById('stats');
 
   const displayMove = function (cell) {
     if (!cell.dataset.checked) {
@@ -114,9 +144,13 @@ const displayController = (function () {
     }
   };
 
+  const displayStats = function (stats) {
+    statsEl.textContent = stats ?? '';
+  };
+
   const initiateGame = function () {
     gameBoardEl.addEventListener('click', function (e) {
-      // listen for a click event only on cells
+      // listen for a click events only on cells
       if (!e.target.classList.contains('cell')) return;
 
       // extract data
@@ -125,14 +159,13 @@ const displayController = (function () {
       const col = e.target.dataset.col;
 
       // avoid playing on same cell twice
-      if (!currentCell.dataset.checked) {
-        play.playOneRound(row, col, currentCell);
+      if (!currentCell.dataset.checked && !game.gameOver && !game.tie) {
+        play.makeMove(row, col, currentCell);
       }
-      console.log(game.board);
     });
   };
 
-  return { initiateGame, displayMove };
+  return { initiateGame, displayMove, displayStats };
 })();
 
 displayController.initiateGame();
